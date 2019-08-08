c3947f9313793d072d5c0bd372dda0966644c5b7
Marco Mornati <ilmorna@gmail.com>
2019-08-08 17:21:53 +0200

    feat: refactored introducing new logger

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 var path = require('path');
 //Overriding configuration folder to be always the fullpath of the scripts
 process.env.NODE_CONFIG_DIR = path.join(__dirname, 'config');
-var config = require('./config/default');
 var utils = require('./lib/utils');
 
 var stdio = require('stdio');
 var op = require('./lib/operations');
 var _ = require('lodash');
 var ncp = require("copy-paste");
+const {Signale} = require('signale');
+const log_config = require('./lib/logConfig');
+const log = new Signale(log_config.options);
 
 var options = stdio.getopt({
-   'verbose': {key: 'v', description: 'Show verbose log', args: 1 },
    'tag': {key: 't', multiple: true, description: 'Tag(s) to allow you to find server'},
    'hostname': {key: 'h', args: 1, description: 'Hostname of your server'},
    'admin_server': {key: 'a', args: 1, description: 'If you need to connect to an Admin server to reach your target. Ex: ssh -tt pi@192.168.0.101 ssh -tt pi2@192.168.0.102'},
@@ -26,9 +27,7 @@ var options = stdio.getopt({
 //Using add the second parameter must be the hostname
 //Using search the second parameter will be a tag by default
 if (options.args && options.args.length > 0) {
-  if (options.verbose) {
     console.log("Overriding parameters...");
-  }
   var args = options.args;
   options.operation = args[0];
   if (args[0] && args[0].toUpperCase() === "ADD") {
@@ -47,10 +46,7 @@ var tags = _.concat([], options.tag ? options.tag : []);
 
 var data = utils.createDataObject(options, {'tags': tags});
 
-if (options.verbose) {
-  console.log(data);
-}
-
+log.info(data);
 var operation = options.operation.toUpperCase();
 
 switch (operation) {
@@ -79,5 +75,5 @@ switch (operation) {
     }
     break;
   default:
-    console.log("Invalid operations. One of " + op.allowed_operations.toString() + " is required");
+    log.warn("Invalid operations. One of " + op.allowed_operations.toString() + " is required");
 }
